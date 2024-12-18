dd246948917ed6a7a9448d0be64bc801dee49f3d
william3mm <90062734+william3mm@users.noreply.github.com>
2024-12-18 19:04:30 +0100

    Add files via upload

---
+++ Biblioteca/src/Classe/AdminWilliam.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classe;
+
+/**
+ *
+ * @author williambersagui
+ */
+public class AdminWilliam {
+    
+    private String nome;
+    
+    private String password ;
+    
+    
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ Biblioteca/src/View/Visible.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author williambersagui
+ */
+public class Visible {
+    
+   
+    
+    public static void main(String [] args){
+        
+//         UsuarioView usuarioview = new UsuarioView();
+//         
+//         usuarioview.setVisible(true);
+         
+       
+         UsuarioView usuario  = new UsuarioView();
+         
+         usuario.setVisible(true);
+                 
+         
+        
+        
+    }
+    
+    
+    
+}
